23ba75aab3b2648a16ac2076357bcf40d5186132
Sébastien Roger <basti.roger@gmail.com>
2024-06-01 13:57:11 +0200

    Initial Commit

---
+++ TheMinute/TheMinuteApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMinuteApp.swift
+//  TheMinute
+//
+//  Created by Sébastien Roger on 01.06.24.
+//
+
+import SwiftUI
+
+@main
+struct TheMinuteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
